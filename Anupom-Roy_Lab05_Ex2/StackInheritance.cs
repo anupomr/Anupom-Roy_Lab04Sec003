49341eb414e949363d35bc4a877c326551949e7e
Anupom Roy <anupomr@yahoo.com>
2018-07-14 18:00:33 -0400

    Implementing a stack

---
+++ Anupom-Roy_Lab05_Ex2/StackInheritance.cs
@@ -0,0 +1,29 @@
+﻿using LinkedListLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anupom_Roy_Lab05_Ex2
+{
+    public class StackInheritance : List
+    {
+        // pass name "stack" to List constructor
+        public StackInheritance() : base("stack") { }
+
+        // place dataValue at top of stack by inserting 
+        // dataValue at front of linked list
+        public void Push(Double dataValue)
+        {
+            InsertAtFront(dataValue);
+        }
+        // remove item from top of stack by removing
+        // item at front of linked list
+        public object Pop()
+        {
+            return RemoveFromFront();
+        }
+
+    }
+}
