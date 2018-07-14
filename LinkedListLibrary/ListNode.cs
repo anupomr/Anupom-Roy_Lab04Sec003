919646e1f2a09b833d18569c952f72f6d23a4281
Anupom Roy <anupomr@yahoo.com>
2018-07-14 17:32:39 -0400

    LinkedList Library is created

---
+++ LinkedListLibrary/ListNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListLibrary
+{
+    // class to represent one node in a list
+
+    class ListNode
+    {
+        // automatic read-only property Data
+        public Double Data { get; private set; }
+
+        // automatic property Next
+        public ListNode Next { get; set; }
+
+        // constructor to create ListNode that refers to dataValue
+        // and is last node in list
+        public ListNode(Double dataValue) : this(dataValue, null) { }
+
+        // constructor to create ListNode that refers to dataValue
+        // and refers to next ListNode in List
+        public ListNode(Double dataValue, ListNode nextNode)
+        {
+            Data = dataValue;
+            Next = nextNode;
+        }
+    }
+}
---
+++ ListTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedListLibrary;
+
+namespace ListTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List(); // create List container
+
+            // create data to store in List
+            bool aBoolean = true;
+            char aCharacter = 'A';
+            Double anDouble = 34567;
+            string aString = "hello";
+
+            // use List insert methods       
+            list.InsertAtFront(anDouble);
+            list.Display();
+            Console.WriteLine($"Number of Elements= {list.Count()}");
+            list.InsertAtFront(aCharacter);
+            list.Display();
+            list.InsertAtBack(22.255);
+            list.Display();
+            list.InsertAtBack(1111111);
+            list.Display();
+            list.Search(1111111);
+            Console.WriteLine("Number of Elements " + list.Count());
+
+            // remove data from list and display after each removal
+            try
+            {
+                object removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromBack();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromBack();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+            }
+            catch (EmptyListException emptyListException)
+            {
+                Console.Error.WriteLine($"\n{emptyListException}");
+            }
+
+        }
+    }
+}
